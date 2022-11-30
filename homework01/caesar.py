503b92b01f6102e09fa45850bb8c107edbd3c073
alexandra2608 <112977143+alexandra2608@users.noreply.github.com>
2022-11-30 16:15:22 +0300

    homework01

---
+++ homework01/caesar.py
@@ -0,0 +1,50 @@
+def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
+    """
+    Encrypts plaintext using a Caesar cipher.
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
+    ciphertext = ""
+    # PUT YOUR CODE HERE
+    for i in range(len(plaintext)):
+        char = plaintext[i]
+        if char.isupper():
+            ciphertext += chr((ord(char) + shift - 65) % 26 + 65)
+        elif char.islower():
+            ciphertext += chr((ord(char) + shift - 97) % 26 + 97)
+        else:
+            ciphertext += plaintext[i]
+    return ciphertext
+
+
+def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
+    """
+    Decrypts a ciphertext using a Caesar cipher.
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
+    plaintext = ""
+    # PUT YOUR CODE HERE
+    for i in range(len(ciphertext)):
+        char = ciphertext[i]
+        if char.isupper():
+            plaintext += chr((ord(char) - shift - 65) % 26 + 65)
+        elif char.islower():
+            plaintext += chr((ord(char) - shift - 97) % 26 + 97)
+        else:
+            plaintext += ciphertext[i]
+    return plaintext
+
+
---
+++ homework01/vigenere.py
@@ -0,0 +1,42 @@
+def encrypt_vigenere(plaintext: str, keyword: str) -> str:
+    """
+    Encrypts plaintext using a Vigenere cipher.
+    >>> encrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> encrypt_vigenere("python", "a")
+    'python'
+    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
+    'LXFOPVEFRNHR'
+    """
+    ciphertext = ""
+    # PUT YOUR CODE HERE
+    for pos, letter in enumerate(plaintext):
+        if letter.isupper():
+            ciphertext += chr(ord("A") + (ord(letter) + (ord(keyword[pos % len(keyword)]) - 2 * ord("A"))) % 26)
+        elif letter.islower():
+            ciphertext += chr(ord("a") + (ord(letter) + (ord(keyword[pos % len(keyword)]) - 2 * ord("a"))) % 26)
+        else:
+            ciphertext += letter
+    return ciphertext
+
+
+def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
+    """
+    Decrypts a ciphertext using a Vigenere cipher.
+    >>> decrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> decrypt_vigenere("python", "a")
+    'python'
+    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
+    'ATTACKATDAWN'
+    """
+    plaintext = ""
+    # PUT YOUR CODE HERE
+    for pos, letter in enumerate(ciphertext):
+        if letter.isupper():
+            plaintext += chr(ord("A") + (ord(letter) - ord(keyword[pos % len(keyword)])) % 26)
+        elif letter.islower():
+            plaintext += chr(ord("a") + (ord(letter) - ord(keyword[pos % len(keyword)])) % 26)
+        else:
+            plaintext += letter
+    return plaintext
